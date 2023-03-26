f7a67d1e61bcbba369a568d85420c27f8e75e065
Alexander Carr <jalexandercarr@gmail.com>
2023-03-26 17:55:17 -0400

    Added google analytics

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -122,7 +122,7 @@ const config = {
   //   limit: 2, // How many posts to display. Max is 10.
   // },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-F8V2VFBWQF', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
